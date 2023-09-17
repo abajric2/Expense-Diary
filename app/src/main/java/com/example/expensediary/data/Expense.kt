8f75aa12fecdb8aca8f522bc990de0e30accb860
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2023-09-17 18:49:03 +0200

    added data class for expenses

---
+++ app/src/main/java/com/example/expensediary/data/Expense.kt
@@ -0,0 +1,24 @@
+package com.example.expensediary.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import java.sql.Date
+
+@Entity(tableName = "expenses",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["id"],
+            childColumns = ["user_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ])
+data class Expense(
+    @PrimaryKey(autoGenerate = true) var id: Long = 0L,
+    @ColumnInfo(name = "user_id") var user_id: Long,
+    @ColumnInfo(name = "item") var item: String,
+    @ColumnInfo(name = "price") var price: Int,
+    @ColumnInfo(name = "expense_date") var expenseDate: Date
+)
