4d7b0824191f8f323b04678ed34dc2fb61d8d14e
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2023-09-22 18:33:31 +0200

    home activity

--- app/src/main/java/com/example/expensediary/HomeActivity.kt
+++ app/src/main/java/com/example/expensediary/HomeActivity.kt
@@ -2,15 +2,17 @@ package com.example.expensediary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.expensediary.data.User
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var selectedDate: TextView
+    private lateinit var datePicker: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val user: User? = intent.getParcelableExtra("user")
-        val pozdrav: String = "Zdravo" + (user?.firstName ?: "")
-        findViewById<TextView>(R.id.hello).text = pozdrav
+
     }
 }
