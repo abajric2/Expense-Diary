f92477abd92bffbc0ce1b7d93b0c6349f04e0544
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2023-09-22 19:55:25 +0200

    date picker implementation in HomeActivity.kt

--- app/src/main/java/com/example/expensediary/HomeActivity.kt
+++ app/src/main/java/com/example/expensediary/HomeActivity.kt
@@ -1,18 +1,40 @@
 package com.example.expensediary
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import com.example.expensediary.data.User
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var selectedDate: TextView
-    private lateinit var datePicker: Button
+    private lateinit var datePicker: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        selectedDate = findViewById(R.id.selectedDate)
+        datePicker = findViewById(R.id.datePicker)
+        updateSelectedDate(Calendar.getInstance())
+        val calendar = Calendar.getInstance()
         val user: User? = intent.getParcelableExtra("user")
-
+        val picker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateSelectedDate(calendar)
+        }
+        datePicker.setOnClickListener {
+            DatePickerDialog(this, picker, calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+    }
+    private fun updateSelectedDate(calendar: Calendar) {
+        val format = "yyyy-MM-dd"
+        val sdf = SimpleDateFormat(format, Locale.UK)
+        selectedDate.text = sdf.format(calendar.time)
     }
 }
