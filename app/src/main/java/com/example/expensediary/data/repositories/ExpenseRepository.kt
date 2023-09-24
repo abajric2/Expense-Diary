f8bb48aa13ec46ebeaadc65a95f2ef975c05d354
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2023-09-24 18:21:32 +0200

    implemented checking if expense for certain user exists

--- app/src/main/java/com/example/expensediary/data/repositories/ExpenseRepository.kt
+++ app/src/main/java/com/example/expensediary/data/repositories/ExpenseRepository.kt
@@ -15,5 +15,20 @@ class ExpenseRepository {
             val expenses: List<Expense> = db!!.expenseDao().getUsersExpensesByDate(user_id, expense_date)
             expenses
         }
+        suspend fun getUsersMonthlySum(user_id: Long, date: String, context: Context): Int = withContext(Dispatchers.IO) {
+            var db = AppDatabase.getInstance(context)
+            val sum: Int = db!!.expenseDao().getUsersMonthlySum(user_id, date)
+            sum
+        }
+        suspend fun getUsersDailySum(user_id: Long, date: String, context: Context): Int = withContext(Dispatchers.IO) {
+            var db = AppDatabase.getInstance(context)
+            val sum: Int = db!!.expenseDao().getUsersDailySum(user_id, date)
+            sum
+        }
+        suspend fun expensesExist(user_id: Long, context: Context): Boolean = withContext(Dispatchers.IO) {
+            var db = AppDatabase.getInstance(context)
+            val expenses: List<Expense> = db!!.expenseDao().getByUsersId(user_id)
+            expenses.isNotEmpty()
+        }
     }
 }
