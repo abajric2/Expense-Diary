6d6ee6258bdf7db29bd48de2f8294ad164d4fcb5
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2023-09-24 18:21:52 +0200

    setting monthly limit info

--- app/src/main/java/com/example/expensediary/HomeActivity.kt
+++ app/src/main/java/com/example/expensediary/HomeActivity.kt
@@ -26,12 +26,17 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var expenseList: RecyclerView
     private lateinit var expenseListAdapter: ExpenseListAdapter
     private lateinit var expenses: List<Expense>
+    private lateinit var monthlyLimitInfo: TextView
+    private lateinit var dailyLimitInfo: TextView
+    private var user: User? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        val user: User? = intent.getParcelableExtra("user")
+        user = intent.getParcelableExtra("user")
         val welcomeMessage: String = "Welcome " + (user?.firstName ?: "")
         findViewById<TextView>(R.id.welcome).text = welcomeMessage
+        monthlyLimitInfo = findViewById(R.id.monthlyLimitInfo)
+        dailyLimitInfo = findViewById(R.id.dailyLimitInfo)
         selectedDate = findViewById(R.id.selectedDate)
         datePicker = findViewById(R.id.datePicker)
         expenses = listOf()
@@ -65,6 +70,30 @@ class HomeActivity : AppCompatActivity() {
         datePicker.setOnClickListener {
             DatePickerDialog(this, picker, calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+        setMonthlyLimitInfo()
+    }
+    private fun setMonthlyLimitInfo() {
+        val context: Context = this
+        val scope = CoroutineScope(Job() + Dispatchers.Main)
+        scope.launch {
+            var info: String = "Your monthly limit is " + user!!.monthlyLimit + " " + user!!.currency + "."
+            if(ExpenseRepository.expensesExist(user!!.id, context)) {
+                var sum = ExpenseRepository.getUsersMonthlySum(
+                    user!!.id,
+                    selectedDate.text.toString(),
+                    context
+                )
+                info += "\n\nUp to this moment, the sum of all expenses during the selected month is " + sum + " " + user!!.currency + "."
+                if (sum > user!!.monthlyLimit) {
+                    info += "\n\nBe careful with your expenses, you have exceeded your monthly limit! You have spent " + (sum - user!!.monthlyLimit) + " " + user!!.currency + " more than you should have."
+                } else {
+                    info += "\n\nYou have " + (user!!.monthlyLimit - sum) + " " + user!!.currency + " left to spend during the selected month if you do not want to exceed the set monthly limit."
+                }
+            } else {
+                info += "\n\nYou haven't spent money so far this month."
+            }
+            monthlyLimitInfo.text = info
         }
     }
     private fun updateSelectedDate(calendar: Calendar) {
